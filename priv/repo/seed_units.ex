ea658ebed8a2247bc9bf7c92fad96a71042c92e8
Rigoberto Perez <rlperez@udel.edu>
2020-09-12 23:18:47 -0400

    Add US units of measure to seed

--- priv/repo/seed_units.ex
+++ priv/repo/seed_units.ex
@@ -9,9 +9,17 @@ defmodule SeedUnits do
     timestamp =
       NaiveDateTime.utc_now()
       |> NaiveDateTime.truncate(:second)
-
+    # Source: https://www.adducation.info/how-to-improve-your-knowledge/units-of-measurement/
     units = [
+      [abbreviation: "tsp", unit: "teaspoon", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "tbsp", unit: "tablespoon", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "oz", unit: "ounces", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
       [abbreviation: "fl oz", unit: "fluid ounce", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "cup", unit: "cup", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "pt", unit: "pint", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "qt", unit: "quart", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "gal", unit: "gallon", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp],
+      [abbreviation: "lb", unit: "pound", standard: "U.S.", inserted_at: timestamp, updated_at: timestamp]
     ]
 
     Repo.insert_all(Unit, units)
