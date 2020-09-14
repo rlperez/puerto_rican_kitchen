3f40986a9ab404ee4402faf8f283b19d5c398bcd
Rigoberto Perez <rlperez@udel.edu>
2020-09-13 22:53:39 -0400

    Add unit seeds back to file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,4 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-SeedUnits.seed()
+alias PuertoRicanKitchen.Repo
+alias PuertoRicanKitchen.Unit
+
+# Units
+Repo.insert! %Unit{abbreviation: "tsp", unit: "teaspoon", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "tbsp", unit: "tablespoon", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "oz", unit: "ounces", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "fl oz", unit: "fluid ounce", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "cup", unit: "cup", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "pt", unit: "pint", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "qt", unit: "quart", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "gal", unit: "gallon", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "lb", unit: "pound", standard: "U.S."}
+Repo.insert! %Unit{abbreviation: "l", unit: "liter", standard: "Metric"}
+Repo.insert! %Unit{abbreviation: "ml", unit: "milliliter", standard: "Metric"}
+Repo.insert! %Unit{abbreviation: "g", unit: "grams", standard: "Metric"}
+Repo.insert! %Unit{abbreviation: "kg", unit: "kilograms", standard: "Metric"}
