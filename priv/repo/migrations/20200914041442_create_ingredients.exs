0a8830c84c0c6b152a8dc6231212d06c61acdcc4
Rigoberto Perez <rlperez@udel.edu>
2020-09-14 00:17:37 -0400

    add ingredients table

---
+++ priv/repo/migrations/20200914041442_create_ingredients.exs
@@ -0,0 +1,12 @@
+defmodule PuertoRicanKitchen.Repo.Migrations.CreateIngredients do
+  use Ecto.Migration
+
+  def change do
+    create table(:ingredients) do
+      add :name, :string
+
+      timestamps()
+    end
+
+  end
+end
